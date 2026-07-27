cac97a9c03ff0750a6b4f946ed4de1c14a29be53
Jhon <88195697+ShonX27@users.noreply.github.com>
2026-07-27 11:43:20 -0700

    Delete check_env.py

--- check_env.py
+++
@@ -1,8 +0,0 @@
-import sys
-import importlib.util
-print('python', sys.executable)
-for name in ['streamlit', 'pandas', 'plotly']:
-    print(name, bool(importlib.util.find_spec(name)))
-    if importlib.util.find_spec(name):
-        mod = __import__(name)
-        print(name, getattr(mod, '__version__', 'unknown'))
